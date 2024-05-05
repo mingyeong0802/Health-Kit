d08d85c7a6a625455d184f72e5eeefc07bcb6f91
GeumSangLEE <dlrmatkd3254@gmail.com>
2024-05-05 20:44:05 +0900

    ver4.0_0505

--- ocr_ver2.0/test_ocr/json_test.py
+++ ocr_ver2.0/test_ocr/json_test.py
@@ -3,9 +3,10 @@ import ipdb
 import pandas as pd
 import re
 
-with open( 'C:\\geumsangLee\\capston\\test_ocr\\ocr_ver2.0\\json_file\\img_hight.json', encoding='utf-8') as file:
+#json 파일 읽어오기
+with open( 'C:\\geumsangLee\\capston\\test_ocr\\ocr_ver2.0\\json_file\\img_json_file.json', encoding='utf-8') as file:
     datas = json.load(file)
-
+#json 파일 접근 코드 -> json 파일에 inferText에 접근하여 영양정보 추출
 field_data= datas['images'][0]['fields']
 text_list = None
 def field_infertext(field_data):
@@ -16,39 +17,20 @@ def field_infertext(field_data):
             field_data[i]['inferText'] = field_data[i]['inferText'].replace(field_data[i]['inferText'],'탄수화물')
         elif ('종 내용당'in field_data[i]['inferText'] or '내용량'in field_data[i]['inferText']):
             field_data[i]['inferText'] = field_data[i]['inferText'].replace(field_data[i]['inferText'],'총 내용량')
+        if ('1일'in field_data[i]['inferText'] or '1일 영양성분'in field_data[i]['inferText']or '1일영양성분'in field_data[i]['inferText']):
+            break
         text_list.append(field_data[i]['inferText'])
-    #return " ".join(text_list)
     return text_list
 infer_sentence=field_infertext(field_data)
-#del infer_sentence[0]
-
 
 
 # 데이터 전처리
-#name = ['총 내용량', '나트륨', '탄수화물', '당류', '지방', '트랜스지방', '포화지방', '콜레스테롤', '단백질', '비타민B1', '비타민B2', '비타민B6', '칼슘', '금상','1일']
+#name은 추가될 수 있음
 name = "나트륨|탄수화물|당류|지방|트랜스지방|포화지방|콜레스테롤|단백질|비타민B1|비타민B2|비타민B6|칼슘"
 result = {}
-
+#딕셔너리 형태로 받아옴 ex) {나트륨:610mg,31%, 탄수화물:54g,17%, ....}
 current_item = None
 current_values = []
-'''
-for item in infer_sentence:
-    m_item=re.match(name, item)
-    if m_item is not None: #만약 item이 name 안에 있다면
-        if current_item is not None: #맨 처음 빼고 가능 
-            if len(current_values) % 2 == 0:
-                current_values = [current_values[i] + current_values[i+1] for i in range(0,len(current_values),2)]
-            result[current_item] = current_values #만약 current_item이 변경되면 딕셔너리 형태로 전체를 저장
-        key_string=m_item.group()
-        value_string=item.replace(key_string, "")
-        current_item = key_string #맨 처음엔 걔가 item으로 변경됨 이후엔 다음 name 요소가 item으로 변경
-        if len(value_string) != 0: 
-            current_values = [value_string] #리스트 초기화
-        else:
-            current_values = []
-    else:
-        current_values.append(item)#name 요소 외에 모두 value로 넣어짐
-'''
 for item in infer_sentence:
     m_item=re.match(name, item)
     if m_item is not None:
@@ -68,27 +50,48 @@ for item in infer_sentence:
 if current_item is not None:
     result[current_item] = current_values
 
-def flatten(lst):
-    return [x for y in lst for x in y]
-
-# g전처리 + 정규표현식 + value 에러 전처리
+#위에 받아온 정보들을 전처리 하고 데이터 프레임으로 받아올 수 있도록 정제
+# g전처리 + 정규표현식 + value 에러 전처리----------------------------
 for key,val in result.items():
     for i,v in enumerate(val):
         if len(val) != 0:
             result[key][i] = re.findall(r'\d+|[a-zA-Z]+|%+', v)
-            #result[key][i] = ''.join(str_list)
-        #if v == 'g':
-        #result[key][i] = '0g'    
-        #if len(val) == 1:
-        #result[key].append('NaN')
-for k_2,v_2 in result.items():
-    result[k_2] = sum(v_2,[])
+    result[key] = sum(val,[])
+        
 
-print(result)
+my_list = []  
+lst_2 = []
+key_name = list(result.keys())[0]
+#ipdb.set_trace()
+for key_2, val_2 in result.items():
+    for idx, v_2 in enumerate(val_2):
+        if len(my_list) == 0:
+            my_list.append(v_2)
+        elif re.search(r'[0-9a-zA-Z]|%', v_2):  
+            if re.search(r'g|mg|%$', my_list[-1]):  
+                join_str = ''.join(my_list)
+                if re.match(r'^0\d.', join_str):
+                    join_str = re.sub(r'^0', '0.', join_str)
+                lst_2.append(join_str)
+                my_list = [] #리셋
+            my_list.append(v_2)
+        if len(lst_2) != 0 and idx == 0:
+            if len(lst_2) == 1 and lst_2 =='g':  
+                lst_2 = '0g'   #'0g'인데 'g'으로만 인식이 되어 '0g'으로 전처리
+            elif len(lst_2) == 1: 
+                lst_2.append('Nan')
+            elif len(lst_2) == 0:
+                lst_2.append('Nan','Nan')
+            result[key_name] = lst_2
+            key_name = key_2
+            lst_2 = [] #리셋
+
+if len(my_list) != 0:
+    join_str = ''.join(my_list)
+    lst_2.append(join_str)
+    result[key_name] = lst_2
 
 
+#데이터 프레임으로 받아오기         
 df = pd.DataFrame(result)
-
-#print(df)
-
-
+print(df)
